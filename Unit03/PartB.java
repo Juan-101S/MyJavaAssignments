ef377417351243dbec03a6b38d5e1c3bac24774e
Juan-101S <26jsandoval@derryfield.org>
2024-10-16 10:48:52 -0400

    Create PartB.java

---
+++ Unit03/PartB.java
@@ -0,0 +1,63 @@
+
+import java.util.Scanner;
+
+public class PartB {
+    public enum DayOfWeek {
+        SUN, MON, TUE, WED, THU, FRI, SAT
+    };
+
+    public static void main(String[] args) throws Exception {
+
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter 1 for Monday, 2 for Tuesday, etc.: ");
+            int reply = in.nextInt();
+            DayOfWeek today;
+
+            System.out.print("What is your age? ");
+            int age = in.nextInt();
+
+            System.out.print("What is the cost of your meal? ");
+            double cost = in.nextDouble();
+
+            double discount = 0.0;
+
+            if (reply == 6) {
+                today = DayOfWeek.SAT;
+            } else if (reply == 1) {
+                today = DayOfWeek.MON;
+            } else if (reply == 2) {
+                today = DayOfWeek.TUE;
+            } else if (reply == 3) {
+                today = DayOfWeek.WED;
+            } else if (reply == 4) {
+                today = DayOfWeek.THU;
+            } else if (reply == 5) {
+                today = DayOfWeek.FRI;
+            } else {
+                today = DayOfWeek.SUN;
+            }
+
+            if (today == DayOfWeek.MON) {
+                if (age < 13) {
+                    discount = 0.075;
+                } else if (age >= 50) {
+                    discount = .15;
+                } else {
+                    discount = .05;
+                }
+            } else if (reply > 1) {
+                discount = 0.0;
+            } else {
+                if (age < 13) {
+                    discount = .05;
+                } else if (age >= 50) {
+                    discount = .075;
+                }
+            }
+
+            double addediscount = cost - (cost * discount);
+            System.out.println("The cost of your meal is $" + addediscount);
+        }
+
+    }
+}
