34ffdae1be68f7da1faff0af8b9bd9d5b14ce495
Juan-101S <26jsandoval@derryfield.org>
2025-02-02 14:40:43 -0500

    homework

---
+++ Unit03/MowerProject/mow/Mower.java
@@ -0,0 +1,92 @@
+package mow;
+
+public class Mower {
+    private int row;
+    private int column;
+    private int direction;
+
+    public Mower(int row, int column, int direction) {
+        this.row = row + 1;
+        this.column = column + 1;
+        this.direction = direction;
+    }
+
+    public boolean updateMower(Yard yard) {
+        if (yard.hasUnmowedGrass() == true) {
+            moveForward();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public void moveForward() {
+        switch (direction) {
+            case 0:
+                row--;
+                break;
+            case 1:
+                column++;
+                break;
+            case 2:
+                row++;
+                break;
+            case 3:
+                column--;
+                break;
+        }
+    }
+
+    public void turnLeft() {
+        direction = (direction + 3) % 4;
+    }
+
+    public void turnRight() {
+        direction = (direction + 1) % 4;
+    }
+
+    public boolean checkGrass(Yard yard) {
+        if (direction == 0) {
+            boolean cell = yard.getStatus(row - 1, column) == '+';
+            return cell;
+        } else if (direction == 1) {
+            boolean cell = yard.getStatus(row, column + 1) == '+';
+            return cell;
+        } else if (direction == 2) {
+            boolean cell = yard.getStatus(row + 1, column) == '+';
+            return cell;
+        } else if (direction == 3) {
+            boolean cell = yard.getStatus(row, column - 1) == '+';
+            return cell;
+        }
+        return false;
+    }
+
+    public void cutGrass(Yard yard) {
+        yard.setCellValue(row, column, 'x');
+    }
+}
---
+++ Unit03/MowerProject/mow/Yard.java
@@ -0,0 +1,75 @@
+package mow;
+
+public class Yard {
+    public char yard[][];
+
+    public Yard(int height, int width) {
+        yard = new char[height + 2][width + 2];
+        for (int i = 0; i < yard.length; i++) {
+            for (int j = 0; j < yard[0].length; j++) {
+                if (i == 0 || i == yard.length - 1 || j == 0 || j == yard[0].length - 1) {
+                    yard[i][j] = '0';
+                } else {
+                    yard[i][j] = '+';
+                }
+            }
+        }
+    }
+
+    public char getStatus(int row, int col) {
+        return yard[row][col];
+    }
+
+    public void setCellValue(int row, int col, char value) {
+        yard[row][col] = value;
+    }
+
+    public int returnHeight() {
+        return yard.length - 2;
+    }
+
+    public int returnLength() {
+        return yard[0].length - 2;
+    }
+
+    public void printYard() {
+        for (int i = 0; i < yard.length; i++) {
+            for (int j = 0; j < yard[0].length; j++) {
+                System.out.print(yard[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void printYard(Mower mower) {
+        for (int i = 0; i < yard.length; i++) {
+            for (int j = 0; j < yard[0].length; j++) {
+                if (mower.getRow() == i && mower.getColumn() == j) { // mower print mower;
+                    if (mower.getDirection() == 0) {
+                        System.out.print("^");
+                    } else if (mower.getDirection() == 1) {
+                        System.out.print(">");
+                    } else if (mower.getDirection() == 2) {
+                        System.out.print("v");
+                    } else if (mower.getDirection() == 3) {
+                        System.out.print("<");
+                    }
+                } else {
+                    System.out.print(yard[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean hasUnmowedGrass() {
+        for (int i = 1; i < yard.length - 1; i++) {
+            for (int j = 1; j < yard[0].length - 1; j++) {
+                if (yard[i][j] == '+') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
