ce11731f54c047440593a795671fec6f85cdd261
Juan-101S <26jsandoval@derryfield.org>
2025-04-09 09:46:42 -0400

    homework

--- Unit03/ProjectileMotionSim/animate/Board.java
+++ Unit03/ProjectileMotionSim/animate/Board.java
@@ -3,20 +3,22 @@ package animate;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
-import java.util.TimerTask;
 import javax.swing.JPanel;
 
 public class Board extends JPanel implements KeyListener {
     private static final int B_WIDTH = 1392;
     private static final int B_HEIGHT = 783;
     private static final int FLOOR = B_HEIGHT - 25;
+    private Cannon cannon;
 
     public Board() {
         setBackground(Color.CYAN);
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
-        this.setFocusable(true);
-        this.addKeyListener(this);
+
+        cannon = new Cannon();
+        cannon.setX(60);
+        cannon.setY(B_HEIGHT - 60);
+        cannon.setAngle(-45);
     }
 
     public void paintComponent(Graphics g) {
@@ -29,6 +31,16 @@ public class Board extends JPanel implements KeyListener {
         g.drawLine(0, FLOOR, B_WIDTH, FLOOR);
         g.setColor(Color.GREEN);
         g.fillRect(0, FLOOR + 1, B_WIDTH, B_HEIGHT - FLOOR);
+
+        cannon.draw(g2d);
+        displayInfo(g2d);
+    }
+
+    private void displayInfo(Graphics2D g2d) {
+        g2d.setColor(Color.RED);
+        g2d.drawString("Press LEFT/RIGHT arrows to adjust the angle", 550, 20);
+        g2d.drawString("Press SPACE to fire", 550, 60);
+        g2d.drawString("Angle = " + (double) Math.round(Math.abs(cannon.getAngle())) + " deg", 550, 80);
     }
 
     public void keyTyped(KeyEvent e) {
@@ -37,12 +49,17 @@ public class Board extends JPanel implements KeyListener {
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             System.out.println("SPACE Key was pressed");
+            cannon.fire();
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             System.out.println("LEFT Key was pressed");
+            cannon.rotateCounterClockwise();
+            repaint();
         }
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             System.out.println("RIGHT Key was pressed");
+            cannon.rotateClockwise();
+            repaint();
         }
         if (e.getKeyCode() == KeyEvent.VK_UP) {
             System.out.println("UP Key was pressed");
@@ -54,5 +71,4 @@ public class Board extends JPanel implements KeyListener {
 
     public void keyReleased(KeyEvent e) {
     }
-
 }
---
+++ Unit03/ProjectileMotionSim/animate/Cannon.java
@@ -0,0 +1,114 @@
+package animate;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.Arrays;
+
+public class Cannon {
+    private int x;
+    private int y;
+    private double angle;
+    private SoundClip wheel;
+    private SoundClip cannon;
+    private BufferedImage cannonimg;
+    private int BASE_WIDTH = 60;
+    private int BASE_HEIGHT = 80;
+    private final int MAX_ANGLE = -90;
+    private final int MIN_ANGLE = 0;
+    private static int MUZZLE_VELOCITY = 30;
+    private static int HYPOT = 100;
+
+    public Cannon() {
+        x = 60;
+        y = 740;
+        angle = Math.toRadians(45);
+        wheel = new SoundClip("media/wheel.wav");
+        wheel.open();
+        cannon = new SoundClip("media/cannon.wav");
+        cannon.open();
+
+        try {
+            File imageFile = new File("media/sm_cannon.png");
+            cannonimg = ImageIO.read(imageFile);
+            System.out.println("Cannon image loaded successfully.");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public void rotateClockwise() {
+        if (angle < MIN_ANGLE) {
+            angle += 1;
+            wheel.play();
+        }
+    }
+
+    public void rotateCounterClockwise() {
+        if (angle > MAX_ANGLE) {
+            angle -= 1;
+            wheel.play();
+        }
+    }
+
+    public void fire() {
+        cannon.play();
+    }
+
+    public void fire(Cannon cannon) {
+        double xo = HYPOT * Math.cos(Math.toRadians(Math.abs(angle)));
+        double yo = HYPOT * Math.sin(Math.toRadians(Math.abs(angle)));
+        double vx = MUZZLE_VELOCITY * Math.cos(Math.toRadians(Math.abs(angle)));
+        double vy = -MUZZLE_VELOCITY * Math.sin(Math.toRadians(Math.abs(angle)));
+        this.fire();
+    }
+
+    public void draw(Graphics2D g2d) {
+        double xpivot = 15;
+        double ypivot = cannonimg.getHeight() / 2.0;
+        AffineTransform af = new AffineTransform();
+        af.translate(x - xpivot, y - ypivot);
+        af.rotate(Math.toRadians(angle), xpivot, ypivot);
+        g2d.drawImage(cannonimg, af, null);
+        g2d.setColor(Color.PINK);
+        int[] xcoordinates = { x, x - (int) (BASE_WIDTH / 2.0), x + (int) (BASE_WIDTH / 2.0) };
+        int[] ycoordinates = { y, y + (int) (BASE_HEIGHT / 2.0), y + (int) (BASE_HEIGHT / 2.0) };
+        g2d.fillPolygon(xcoordinates, ycoordinates, 3);
+        System.out.println(Arrays.toString(xcoordinates));
+        System.out.println(Arrays.toString(ycoordinates));
+        g2d.setColor(Color.BLACK);
+        int[] xxcoordinates = { x, x - (int) (BASE_WIDTH / 2.0), x + (int) (BASE_WIDTH / 2.0) };
+        int[] yycoordinates = { y, y + (int) (BASE_HEIGHT / 2.0), y + (int) (BASE_HEIGHT / 2.0) };
+        g2d.drawPolygon(xxcoordinates, yycoordinates, 3);
+        System.out.println(Arrays.toString(xxcoordinates));
+        System.out.println(Arrays.toString(yycoordinates));
+        g2d.setColor(Color.BLUE);
+        g2d.fillOval((int) (x - 5), (int) (y - 5), 10, 10);
+    }
+}
