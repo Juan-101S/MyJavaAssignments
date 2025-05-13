65a9d930484e1375321c36a355914e860c3f21ab
Juan-101S <26jsandoval@derryfield.org>
2025-05-12 22:56:29 -0400

    homework

--- Unit03/GUIDemo/Board.java
+++ Unit03/GUIDemo/Board.java
@@ -2,15 +2,18 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Board extends JPanel {
-    private final int B_WIDTH = 450;
-    private final int B_HEIGHT = 100;
-    private final int TEXT_WIDTH = 10;
+    private final int B_WIDTH = 400;
+    private final int B_HEIGHT = 250;
 
     private final double INIT_BALANCE = 1000.00;
     private final double INIT_APR = 5.0;
@@ -18,30 +21,38 @@ public class Board extends JPanel {
     JButton button;
     JLabel labelInterest;
     JTextField textRate;
-    JLabel labelBalance;
+    JTextArea textArea;
+    JScrollPane scrollPane;
 
     double balance;
 
     // constructor.
     public Board() {
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+        JPanel inputPanel = new JPanel();
         labelInterest = new JLabel("Interest Rate (%):");
-        this.add(labelInterest);
-
-        textRate = new JTextField(TEXT_WIDTH);
-        this.add(textRate);
-        String text = Double.toString(INIT_APR);
-        textRate.setText(text);
-
+        inputPanel.add(labelInterest);
+        textRate = new JTextField(10);
+        textRate.setText(String.valueOf(INIT_APR));
+        inputPanel.add(textRate);
         button = new JButton("Add Interest");
-        this.add(button);
         button.addActionListener(new ClickMeListener());
+        inputPanel.add(button);
+        add(inputPanel);
 
+        textArea = new JTextArea(10, 30);
+        textArea.setEditable(false);
+        scrollPane = new JScrollPane(textArea);
+        add(scrollPane);
         balance = INIT_BALANCE;
-        text = String.format("Balance ($): %.2f", balance);
-        labelBalance = new JLabel(text);
-        this.add(labelBalance);
+        appendBalanceText();
+    }
+
+    private void appendBalanceText() {
+        String text = String.format("Balance ($): %.2f\n", balance);
+        textArea.append(text);
     }
 
     // this inner class is used to listen for mouse clicks.
@@ -49,13 +60,15 @@ public class Board extends JPanel {
         @Override
         public void actionPerformed(ActionEvent e) {
             System.out.println("Clicked!");
-
-            double rate = Double.parseDouble(textRate.getText());
-            rate = rate / 100.0;
-            balance = balance + (rate * balance);
-
-            String text = String.format("Balance ($): %.2f", balance);
-            labelBalance.setText(text);
+            try {
+                double rate = Double.parseDouble(textRate.getText());
+                rate = rate / 100.0;
+                balance += (rate * balance);
+                appendBalanceText();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(Board.this, "Please enter a valid number for the interest rate.",
+                        "Input Error", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 }
